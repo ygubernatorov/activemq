6187cf43f7bfd05eadf677a6e2038e2ad6f4bb61
Étienne H <ehossack@amazon.com>
2021-11-09 18:33:41 -0800

    [AMQ-8354] Add ReplicationQueueSupplier
    This adds the initial replication queue and blocks
    other access to the resource until it is fully
    initialized

---
+++ activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaSupport.java
@@ -0,0 +1,5 @@
+package org.apache.activemq.replica;
+
+public class ReplicaSupport {
+    public static final String REPLICATION_QUEUE_PREFIX = "replication_plugin_queue";
+}
---
+++ activemq-broker/src/test/java/org/apache/activemq/replica/ReplicaReplicationQueueSupplierTest.java
@@ -0,0 +1,14 @@
+package org.apache.activemq.replica;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ReplicaReplicationQueueSupplierTest {
+
+    @Test
+    public void canCreateQueue() {
+        fail("Not yet implemented");
+    }
+
+}
